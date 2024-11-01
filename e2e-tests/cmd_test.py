2475ff03ef6fe33a4fbcd958c779b65dad6f378b
Jordi Mas <jmas@softcatala.org>
2024-11-01 16:47:43 +0100

    More comprehensive end to end test

--- e2e-tests/cmd_test.py
+++ e2e-tests/cmd_test.py
@@ -10,7 +10,9 @@ from faster_whisper import WhisperModel
 
 class TestCmd:
 
-    # TODO: To check transcription out of the final video
+    # TODO:
+    #   - To check transcription out of the final video
+    #   - Check speed in macOS
     def _get_transcription(self, filename):
         model = WhisperModel("medium")
         segments, info = model.transcribe(filename, language="ca", temperature=[0])
@@ -25,36 +27,80 @@ class TestCmd:
         path, _ = os.path.split(full_path)
 
         _file = os.path.join(path, "englishvideo.mp4")
-        with tempfile.TemporaryDirectory() as directory:
-            command = (
-                "open-dubbing "
-                f"--input_file={_file} "
-                f"--output_directory={directory} "
-                "--source_language=eng "
-                "--target_language=cat "
-                "--nllb_model=nllb-200-1.3B "
-                "--whisper_model=medium "
-                f"--tts={tts_engine}"
-            )
-            cmd = f"cd {directory} && {command}"
-            os.system(cmd)
-            operating = platform.system().lower()
+        dir_obj = tempfile.TemporaryDirectory()
+        directory = dir_obj.name
+        command = (
+            "open-dubbing "
+            f"--input_file={_file} "
+            f"--output_directory={directory} "
+            "--source_language=eng "
+            "--target_language=cat "
+            "--nllb_model=nllb-200-1.3B "
+            "--whisper_model=medium "
+            f"--tts={tts_engine}"
+        )
+        cmd = f"cd {directory} && {command}"
+        os.system(cmd)
+        operating = platform.system().lower()
 
-            # ['- Bon dia. - Bé.', 'El meu nom és Jordi Mas.', 'Sóc de Barcelona.', "I m'encanta aquesta ciutat."]
-            metadata_file = os.path.join(directory, "utterance_metadata_cat.json")
-            with open(metadata_file, encoding="utf-8") as json_data:
-                data = json.load(json_data)
-                utterances = data["utterances"]
-                text_array = [entry["translated_text"] for entry in utterances]
-                print(f"text_array: {text_array}")
-                if operating == "darwin":
-                    assert "- Bon dia. - Bé." == text_array[0]
-                    assert "El meu nom és Jordi Mas." == text_array[1]
-                    assert "Sóc de Barcelona." == text_array[2]
-                    assert "I m'encanta aquesta ciutat." == text_array[3]
-                else:
-                    assert "Bon dia, em dic Jordi Mas." == text_array[0]
-                    assert "Sóc de Barcelona." == text_array[1]
-                    assert "I m'encanta aquesta ciutat." == text_array[2]
+        metadata_file = os.path.join(directory, "utterance_metadata_cat.json")
+        with open(metadata_file, encoding="utf-8") as json_data:
+            data = json.load(json_data)
+            utterances = data["utterances"]
+            text_array = [entry["translated_text"] for entry in utterances]
 
-                assert all("Male" == entry["gender"] for entry in utterances)
+            # Common
+            assert all(
+                "Male" == entry["gender"] for entry in utterances
+            ), "Utterance gender check failed"
+
+            starts = [entry["start"] for entry in utterances]
+            ends = [entry["end"] for entry in utterances]
+            speeds = [entry["speed"] for entry in utterances]
+
+            if operating == "darwin":
+                assert 4 == len(utterances)
+                assert [
+                    1.26284375,
+                    2.44409375,
+                    5.24534375,
+                    7.607843750000001,
+                ] == starts, "Utterance start check failed"
+
+                assert [
+                    2.17409375,
+                    3.94596875,
+                    6.61221875,
+                    8.687843750000003,
+                ] == ends, "Utterance end check failed"
+
+                assert "- Bon dia. - Bé." == text_array[0], "translated text 0"
+                assert "El meu nom és Jordi Mas." == text_array[1], "translated text 1"
+                assert "Sóc de Barcelona." == text_array[2], "translated text 2"
+                assert (
+                    "I m'encanta aquesta ciutat." == text_array[3]
+                ), "translated text 3"
+
+            else:
+                assert 3 == len(utterances)
+                assert [
+                    1.26284375,
+                    5.24534375,
+                    7.607843750000001,
+                ] == starts, "Utterance start check failed"
+
+                assert [
+                    3.94596875,
+                    6.629093750000001,
+                    8.687843750000003,
+                ] == ends, "Utterance end check failed"
+
+                assert [1.0, 1.0, 1.3] == speeds, "Utterance speed check failed"
+
+                assert (
+                    "Bon dia, em dic Jordi Mas." == text_array[0]
+                ), "translated text 0"
+                assert "Sóc de Barcelona." == text_array[1], "translated text 1"
+                assert (
+                    "I m'encanta aquesta ciutat." == text_array[2]
+                ), "translated text 2"
